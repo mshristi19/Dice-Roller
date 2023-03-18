8693cf11e542f460392bfd6e0c61e5ea65f576fe
mshristi19 <mshristi19@tbc.edu.np>
2023-03-18 18:23:33 +0545

    Added Functionality

--- app/src/main/java/com/example/diceroller/MainActivity.java
+++ app/src/main/java/com/example/diceroller/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.diceroller;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final Random RANDOM = new Random();
+    private Button rollButton;
+    private TextView resultText;
+    int showCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rollButton = findViewById(R.id.button);
+        resultText = findViewById(R.id.resultTextView);
+        rollButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showCount++;
+                if(resultText != null)
+                    resultText.setText(Integer.toString(showCount));
+            }
+        });
     }
 }
